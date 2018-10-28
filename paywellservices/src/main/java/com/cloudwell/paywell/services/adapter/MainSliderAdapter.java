c91621a2fcb233db75baf118f36ef593f25a47ac
Kazi Md. Saidul <kazi.saidul@cloudwell.co>
2018-10-28 11:22:52 +0600

    Add new Banner-Slider libaray for home page image Slider

---
+++ paywellservices/src/main/java/com/cloudwell/paywell/services/adapter/MainSliderAdapter.java
@@ -0,0 +1,35 @@
+package com.cloudwell.paywell.services.adapter;
+
+import android.content.Context;
+
+import ss.com.bannerslider.adapters.SliderAdapter;
+import ss.com.bannerslider.viewholder.ImageSlideViewHolder;
+
+/**
+ * Created by Kazi Md. Saidul Email: Kazimdsaidul@gmail.com  Mobile: +8801675349882 on 10/28/18.
+ */
+public class MainSliderAdapter extends SliderAdapter {
+
+    private final Context context;
+    private final String[] imageUrls;
+
+    public MainSliderAdapter(Context context, String[] imageUrls) {
+        this.context = context;
+        this.imageUrls = imageUrls;
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return imageUrls.length;
+    }
+
+    @Override
+    public void onBindImageSlide(int position, ImageSlideViewHolder viewHolder) {
+
+        String imageUrl = imageUrls[position];
+        viewHolder.bindImageSlide(imageUrl);
+
+
+    }
+}
