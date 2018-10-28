79c28cccf61cff76f1661ca60cabb075bbc14515
Kazi Md. Saidul <kazi.saidul@cloudwell.co>
2018-10-28 11:57:23 +0600

    Update image cache logic 24h

--- paywellservices/src/main/java/com/cloudwell/paywell/services/adapter/PicassoImageLoadingService.java
+++ paywellservices/src/main/java/com/cloudwell/paywell/services/adapter/PicassoImageLoadingService.java
@@ -3,6 +3,9 @@ package com.cloudwell.paywell.services.adapter;
 import android.content.Context;
 import android.widget.ImageView;
 
+import com.cloudwell.paywell.services.app.AppHandler;
+import com.squareup.picasso.MemoryPolicy;
+import com.squareup.picasso.NetworkPolicy;
 import com.squareup.picasso.Picasso;
 
 import ss.com.bannerslider.ImageLoadingService;
@@ -14,23 +17,73 @@ import ss.com.bannerslider.ImageLoadingService;
 
 public class PicassoImageLoadingService implements ImageLoadingService {
     public Context context;
+    public static final long UPDATE_IMG_CACHE_CLEAN_INTERVAL = 7 * 24 * 60 * 60;// 1 day
+    private AppHandler mAppHandler;
+
 
     public PicassoImageLoadingService(Context context) {
         this.context = context;
+        mAppHandler = new AppHandler(context);
     }
 
     @Override
     public void loadImage(String url, ImageView imageView) {
-        Picasso.get().load(url).into(imageView);
+        if ((System.currentTimeMillis() / 1000) >= (mAppHandler.getImgCacheCleanUpdateCheck() + UPDATE_IMG_CACHE_CLEAN_INTERVAL)) {
+            mAppHandler.setImgCacheCleanUpdateCheck(System.currentTimeMillis() / 1000);
+            Picasso.get()
+                    .load(url)
+                    .fit()
+                    .centerCrop()
+                    .memoryPolicy(MemoryPolicy.NO_CACHE)
+                    .networkPolicy(NetworkPolicy.NO_CACHE)
+                    .into(imageView);
+        } else {
+            Picasso.get()
+                    .load(url)
+                    .fit()
+                    .centerCrop()
+                    .into(imageView);
+        }
+
     }
 
     @Override
     public void loadImage(int resource, ImageView imageView) {
-        Picasso.get().load(resource).into(imageView);
+        if ((System.currentTimeMillis() / 1000) >= (mAppHandler.getImgCacheCleanUpdateCheck() + UPDATE_IMG_CACHE_CLEAN_INTERVAL)) {
+            mAppHandler.setImgCacheCleanUpdateCheck(System.currentTimeMillis() / 1000);
+            Picasso.get()
+                    .load(resource)
+                    .fit()
+                    .centerCrop()
+                    .memoryPolicy(MemoryPolicy.NO_CACHE)
+                    .networkPolicy(NetworkPolicy.NO_CACHE)
+                    .into(imageView);
+        } else {
+            Picasso.get()
+                    .load(resource)
+                    .fit()
+                    .centerCrop()
+                    .into(imageView);
+        }
     }
 
     @Override
     public void loadImage(String url, int placeHolder, int errorDrawable, ImageView imageView) {
-        Picasso.get().load(url).placeholder(placeHolder).error(errorDrawable).into(imageView);
+        if ((System.currentTimeMillis() / 1000) >= (mAppHandler.getImgCacheCleanUpdateCheck() + UPDATE_IMG_CACHE_CLEAN_INTERVAL)) {
+            mAppHandler.setImgCacheCleanUpdateCheck(System.currentTimeMillis() / 1000);
+            Picasso.get()
+                    .load(url)
+                    .fit()
+                    .centerCrop()
+                    .memoryPolicy(MemoryPolicy.NO_CACHE)
+                    .networkPolicy(NetworkPolicy.NO_CACHE)
+                    .into(imageView);
+        } else {
+            Picasso.get()
+                    .load(url)
+                    .fit()
+                    .centerCrop()
+                    .into(imageView);
+        }
     }
 }
