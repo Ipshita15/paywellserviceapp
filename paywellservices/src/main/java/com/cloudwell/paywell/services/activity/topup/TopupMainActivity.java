72dc5d730e935eae744184606e3080753f947ded
naima.gani <naima.gani@cloudwell.co>
2018-10-29 17:07:07 +0600

    Topup bundle offer routing according to operators

--- paywellservices/src/main/java/com/cloudwell/paywell/services/activity/topup/TopupMainActivity.java
+++ paywellservices/src/main/java/com/cloudwell/paywell/services/activity/topup/TopupMainActivity.java
@@ -36,6 +36,7 @@ import android.widget.RadioGroup;
 import android.widget.RelativeLayout;
 
 import com.cloudwell.paywell.services.R;
+import com.cloudwell.paywell.services.activity.topup.offer.OfferMainActivity;
 import com.cloudwell.paywell.services.activity.utility.ivac.DrawableClickListener;
 import com.cloudwell.paywell.services.app.AppController;
 import com.cloudwell.paywell.services.app.AppHandler;
@@ -49,6 +50,8 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -80,6 +83,12 @@ public class TopupMainActivity extends AppCompatActivity implements View.OnClick
     private RadioButton radioButton_five, radioButton_ten, radioButton_twenty, radioButton_fifty, radioButton_hundred, radioButton_twoHundred;
     private String selectedLimit = "";
 
+    String key;
+    private int operator_number;
+    private static final String TAG_STATUS = "status";
+    private static final String TAG_RECHARGE_OFFER = "RechargeOffer";
+    private static final String TAG_MESSAGE = "message";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -87,7 +96,7 @@ public class TopupMainActivity extends AppCompatActivity implements View.OnClick
 
         setContentView(R.layout.activity_topup_main);
 
-        String key = getIntent().getStringExtra("key");
+        key = getIntent().getStringExtra("key");
         Log.d("Key", key);
 
         assert getSupportActionBar() != null;
@@ -166,7 +175,8 @@ public class TopupMainActivity extends AppCompatActivity implements View.OnClick
         } else if (v.getId() == R.id.enquiryBtn) {
             showEnquiryPrompt();
         } else if (v.getId() == R.id.imageOffer) {
-            startActivity(new Intent(TopupMainActivity.this, OperatorMenuActivity.class));
+//            startActivity(new Intent(TopupMainActivity.this, OperatorMenuActivity.class));
+            onClickBundleOffer(key);
         }
     }
 
@@ -1086,4 +1096,159 @@ public class TopupMainActivity extends AppCompatActivity implements View.OnClick
         finish();
     }
 
+
+    public void onClickBundleOffer(String operator) {
+        switch (operator) {
+            case "GP":
+                if (cd.isConnectingToInternet()) {
+                    operator_number = 1;
+                    new OfferInquiryAsync().execute(getResources().getString(R.string.topup_offer), String.valueOf(operator_number));
+                } else {
+                    Snackbar snackbar = Snackbar.make(mRelativeLayout, R.string.connection_error_msg, Snackbar.LENGTH_LONG);
+                    snackbar.setActionTextColor(Color.parseColor("#ffffff"));
+                    View snackBarView = snackbar.getView();
+                    snackBarView.setBackgroundColor(Color.parseColor("#4CAF50"));
+                    snackbar.show();
+                }
+                break;
+            case "ROBI":
+                if (cd.isConnectingToInternet()) {
+                    operator_number = 3;
+                    new OfferInquiryAsync().execute(getResources().getString(R.string.topup_offer), String.valueOf(operator_number));
+                } else {
+                    Snackbar snackbar = Snackbar.make(mRelativeLayout, R.string.connection_error_msg, Snackbar.LENGTH_LONG);
+                    snackbar.setActionTextColor(Color.parseColor("#ffffff"));
+                    View snackBarView = snackbar.getView();
+                    snackBarView.setBackgroundColor(Color.parseColor("#4CAF50"));
+                    snackbar.show();
+                }
+                break;
+            case "BANGLALINK":
+                if (cd.isConnectingToInternet()) {
+                    operator_number = 2;
+                    new OfferInquiryAsync().execute(getResources().getString(R.string.topup_offer), String.valueOf(operator_number));
+                } else {
+                    Snackbar snackbar = Snackbar.make(mRelativeLayout, R.string.connection_error_msg, Snackbar.LENGTH_LONG);
+                    snackbar.setActionTextColor(Color.parseColor("#ffffff"));
+                    View snackBarView = snackbar.getView();
+                    snackBarView.setBackgroundColor(Color.parseColor("#4CAF50"));
+                    snackbar.show();
+                }
+                break;
+            case "TELETALK":
+                if (cd.isConnectingToInternet()) {
+                    operator_number = 6;
+                    new OfferInquiryAsync().execute(getResources().getString(R.string.topup_offer), String.valueOf(operator_number));
+                } else {
+                    Snackbar snackbar = Snackbar.make(mRelativeLayout, R.string.connection_error_msg, Snackbar.LENGTH_LONG);
+                    snackbar.setActionTextColor(Color.parseColor("#ffffff"));
+                    View snackBarView = snackbar.getView();
+                    snackBarView.setBackgroundColor(Color.parseColor("#4CAF50"));
+                    snackbar.show();
+                }
+                break;
+            case "AIRTEL":
+                if (cd.isConnectingToInternet()) {
+                    operator_number = 4;
+                    new OfferInquiryAsync().execute(getResources().getString(R.string.topup_offer), String.valueOf(operator_number));
+                } else {
+                    Snackbar snackbar = Snackbar.make(mRelativeLayout, R.string.connection_error_msg, Snackbar.LENGTH_LONG);
+                    snackbar.setActionTextColor(Color.parseColor("#ffffff"));
+                    View snackBarView = snackbar.getView();
+                    snackBarView.setBackgroundColor(Color.parseColor("#4CAF50"));
+                    snackbar.show();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    private class OfferInquiryAsync extends AsyncTask<String, Void, String> {
+        private ProgressDialog progressDialog;
+
+        @Override
+        protected void onPreExecute() {
+            progressDialog = ProgressDialog.show(TopupMainActivity.this, "", getString(R.string.loading_msg), true);
+            if (!isFinishing())
+                progressDialog.show();
+        }
+
+        @SuppressWarnings("deprecation")
+        @Override
+        protected String doInBackground(String... data) {
+            String responseTxt = null;
+            // Create a new HttpClient and Post Header
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost(data[0]);
+            try {
+                //add data
+                List<NameValuePair> nameValuePairs = new ArrayList<>(3);
+                nameValuePairs.add(new BasicNameValuePair("imei", mAppHandler.getImeiNo()));
+                nameValuePairs.add(new BasicNameValuePair("subServiceId", data[1]));
+                nameValuePairs.add(new BasicNameValuePair("format", "json"));
+                httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+                ResponseHandler<String> responseHandler = new BasicResponseHandler();
+                responseTxt = httpclient.execute(httppost, responseHandler);
+            } catch (Exception e) {
+                e.printStackTrace();
+                Snackbar snackbar = Snackbar.make(mRelativeLayout, R.string.try_again_msg, Snackbar.LENGTH_LONG);
+                snackbar.setActionTextColor(Color.parseColor("#ffffff"));
+                View snackBarView = snackbar.getView();
+                snackBarView.setBackgroundColor(Color.parseColor("#4CAF50"));
+            }
+            return responseTxt;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            progressDialog.cancel();
+            try {
+                if (result != null) {
+                    JSONObject jsonObject = new JSONObject(result);
+                    String status = jsonObject.getString(TAG_STATUS);
+                    if (status.equals("200")) {
+                        JSONArray array = jsonObject.getJSONArray(TAG_RECHARGE_OFFER);
+                        Bundle bundle = new Bundle();
+                        bundle.putString("array", array.toString());
+                        if (operator_number == 1) {
+                            OfferMainActivity.operatorName = getString(R.string.home_gp);
+                        } else if (operator_number == 2) {
+                            OfferMainActivity.operatorName = getString(R.string.home_bl);
+                        } else if (operator_number == 3) {
+                            OfferMainActivity.operatorName = getString(R.string.home_rb);
+                        } else if (operator_number == 4) {
+                            OfferMainActivity.operatorName = getString(R.string.home_at);
+                        } else if (operator_number == 6) {
+                            OfferMainActivity.operatorName = getString(R.string.home_tt);
+                        }
+                        Intent intent = new Intent(TopupMainActivity.this, OfferMainActivity.class);
+                        intent.putExtras(bundle);
+                        startActivity(intent);
+//                        finish();
+                    } else {
+                        Snackbar snackbar = Snackbar.make(mRelativeLayout, jsonObject.getString(TAG_MESSAGE), Snackbar.LENGTH_LONG);
+                        snackbar.setActionTextColor(Color.parseColor("#ffffff"));
+                        View snackBarView = snackbar.getView();
+                        snackBarView.setBackgroundColor(Color.parseColor("#4CAF50"));
+                        snackbar.show();
+                    }
+                } else {
+                    Snackbar snackbar = Snackbar.make(mRelativeLayout, R.string.try_again_msg, Snackbar.LENGTH_LONG);
+                    snackbar.setActionTextColor(Color.parseColor("#ffffff"));
+                    View snackBarView = snackbar.getView();
+                    snackBarView.setBackgroundColor(Color.parseColor("#4CAF50"));
+                    snackbar.show();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                Snackbar snackbar = Snackbar.make(mRelativeLayout, R.string.try_again_msg, Snackbar.LENGTH_LONG);
+                snackbar.setActionTextColor(Color.parseColor("#ffffff"));
+                View snackBarView = snackbar.getView();
+                snackBarView.setBackgroundColor(Color.parseColor("#4CAF50"));
+                snackbar.show();
+            }
+        }
+    }
 }
