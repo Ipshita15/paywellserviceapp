8bb72c7ab2390be2c9368d450fad92856558dab3
Kazi md. Saidul <kazi.saidul@cloudwell.co>
2018-12-04 15:49:05 +0600

    Bug fixed : Notification details ScrollView

--- paywellservices/src/main/java/com/cloudwell/paywell/services/activity/notification/ImageViewActivity.java
+++ paywellservices/src/main/java/com/cloudwell/paywell/services/activity/notification/ImageViewActivity.java
@@ -2,19 +2,24 @@ package com.cloudwell.paywell.services.activity.notification;
 
 import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.Nullable;
 import android.view.MenuItem;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.RequestOptions;
 import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.target.Target;
 import com.bumptech.glide.request.transition.Transition;
 import com.cloudwell.paywell.services.R;
+import com.cloudwell.paywell.services.activity.base.BaseActivity;
 import com.davemorrissey.labs.subscaleview.ImageSource;
 import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
 
-public class ImageViewActivity extends AppCompatActivity {
+public class ImageViewActivity extends BaseActivity {
 
     public static String TAG_IMAGE_URL = "image";
 
@@ -29,8 +34,23 @@ public class ImageViewActivity extends AppCompatActivity {
         }
         final SubsamplingScaleImageView imageView = findViewById(R.id.imageViewNotification);
 
+        showProgressDialog();
+
         Glide.with(ImageViewActivity.this)
                 .asBitmap()
+                .listener(new RequestListener<Bitmap>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Bitmap> target, boolean isFirstResource) {
+                        dismissProgressDialog();
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Bitmap resource, Object model, Target<Bitmap> target, DataSource dataSource, boolean isFirstResource) {
+                        dismissProgressDialog();
+                        return false;
+                    }
+                })
                 .load(TAG_IMAGE_URL)
                 .apply(RequestOptions.diskCacheStrategyOf(DiskCacheStrategy.AUTOMATIC))
                 .into(new SimpleTarget<Bitmap>() {
