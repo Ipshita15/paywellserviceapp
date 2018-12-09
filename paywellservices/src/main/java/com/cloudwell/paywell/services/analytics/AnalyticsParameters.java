28ee3a534ad8c218895569fe966283837ae9cfa5
Kazi md. Saidul <kazi.saidul@cloudwell.co>
2018-12-09 17:39:41 +0600

    Add AnalyticsManager for center analytics data

---
+++ paywellservices/src/main/java/com/cloudwell/paywell/services/analytics/AnalyticsParameters.java
@@ -0,0 +1,15 @@
+package com.cloudwell.paywell.services.analytics;
+
+/**
+ * Created by Kazi Md. Saidul Email: Kazimdsaidul@gmail.com  Mobile: +8801675349882 on 9/12/18.
+ */
+public class AnalyticsParameters {
+    public static final String KEY_ACTION = "Action";
+    public static final String KEY_LABEL = "Label";
+    public static final String KEY_VALUE = "Value";
+
+
+    //
+    public static final String KEY_DASHBOARD = "Dashboard";
+    public static String KEY_TOPUP_MENU = "TopupMenu";
+}
