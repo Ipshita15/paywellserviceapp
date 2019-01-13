d1dcac098a6d930a6585ab84475c1907433d3550
Kazi md. Saidul <kazi.saidul@cloudwell.co>
2019-01-13 13:35:38 +0600

    Remove some old unuse code

--- custom-calendar/src/main/java/com/cloudwell/paywell/customcalendarview/CalendarListener.java
+++
@@ -1,9 +0,0 @@
-package com.cloudwell.paywell.customcalendarview;
-
-import java.util.Date;
-
-public interface CalendarListener {
-    void onDateSelected(Date date);
-
-    void onMonthChanged(Date time);
-}
