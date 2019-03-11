6400cd040343acb0adfb5328316fc35d0f694ad9
Kazi md. Saidul <kazi.saidul@cloudwell.co>
2019-03-11 17:52:26 +0600

    Refactoring code

--- paywellservices/src/main/java/com/cloudwell/paywell/services/activity/eticket/airticket/menu/AirTicketMenuActivity.kt
+++ paywellservices/src/main/java/com/cloudwell/paywell/services/activity/eticket/airticket/menu/AirTicketMenuActivity.kt
@@ -6,6 +6,8 @@ import android.view.View
 import com.cloudwell.paywell.services.R
 import com.cloudwell.paywell.services.activity.base.AirTricketBaseActivity
 import com.cloudwell.paywell.services.activity.eticket.airticket.AirTicketMainActivity
+import com.cloudwell.paywell.services.activity.eticket.airticket.bookingCencel.BookingCancelActivity
+import com.cloudwell.paywell.services.activity.eticket.airticket.transationLog.TransactionLogActivity
 import kotlinx.android.synthetic.main.activity_air_tricket_menu.*
 
 class AirTicketMenuActivity : AirTricketBaseActivity(), View.OnClickListener {
@@ -15,12 +17,23 @@ class AirTicketMenuActivity : AirTricketBaseActivity(), View.OnClickListener {
         setContentView(R.layout.activity_air_tricket_menu)
         setToolbar(getString(R.string.home_eticket_air))
         btSerach.setOnClickListener(this)
+        btCencel.setOnClickListener(this)
+        btTransationLog.setOnClickListener(this)
+
     }
 
     override fun onClick(v: View?) {
         when (v?.id) {
             R.id.btSerach -> {
-                startActivity(Intent(application, AirTicketMainActivity::class.java))
+                startActivity(Intent(applicationContext, AirTicketMainActivity::class.java))
+            }
+
+            R.id.btCencel -> {
+                startActivity(Intent(applicationContext, BookingCancelActivity::class.java))
+            }
+
+            R.id.btTransationLog -> {
+                startActivity(Intent(applicationContext, TransactionLogActivity::class.java))
             }
         }
     }
