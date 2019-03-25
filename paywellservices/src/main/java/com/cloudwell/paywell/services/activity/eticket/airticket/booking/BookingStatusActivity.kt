d1ba1eebd6bcf05052f7eb2aecb55e9a518a0a59
Kazi md. Saidul <kazi.saidul@cloudwell.co>
2019-03-25 18:35:12 +0600

    Day end commit 25-03-2019

--- paywellservices/src/main/java/com/cloudwell/paywell/services/activity/eticket/airticket/booking/BookingStatusActivity.kt
+++ paywellservices/src/main/java/com/cloudwell/paywell/services/activity/eticket/airticket/booking/BookingStatusActivity.kt
@@ -1,23 +1,24 @@
 package com.cloudwell.paywell.services.activity.eticket.airticket.booking
 
-import android.content.Context
+import android.content.DialogInterface
 import android.graphics.Color
 import android.graphics.Typeface
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.widget.DefaultItemAnimator
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
-import android.view.*
+import android.view.Menu
+import android.view.MenuItem
+import com.cloudwell.paywell.services.R
 import com.cloudwell.paywell.services.activity.base.AirTricketBaseActivity
 import com.cloudwell.paywell.services.activity.eticket.airticket.booking.model.BookingList
-import com.cloudwell.paywell.services.activity.myFavorite.model.FavoriteMenu
+import com.cloudwell.paywell.services.activity.eticket.airticket.booking.model.Datum
+import com.cloudwell.paywell.services.activity.eticket.airticket.bookingStatus.BookingStatusListAdapter
+import com.cloudwell.paywell.services.activity.eticket.airticket.bookingStatus.ItemClickListener
 import com.cloudwell.paywell.services.app.storage.AppStorageBox
 import kotlinx.android.synthetic.main.activity_booking_main.*
 import kotlinx.android.synthetic.main.item_booking_status.*
-import kotlinx.android.synthetic.main.test.view.*
-import java.text.SimpleDateFormat
-import java.util.*
 
 
 class BookingStatusActivity : AirTricketBaseActivity() {
@@ -46,9 +47,14 @@ class BookingStatusActivity : AirTricketBaseActivity() {
     }
 
     private fun initViewInitialization() {
-        val customAdapter = MyAdapter(this)
-        listBookingList.adapter = customAdapter
+        val customAdapter = BookingStatusListAdapter(responseList, this.applicationContext, object : ItemClickListener {
+            override fun onItemClick(position: Datum) {
+                showTricketIntentPopupMessage()
+            }
 
+        })
+
+        listBookingList.adapter = customAdapter
 
         val mLayoutManager = LinearLayoutManager(applicationContext)
         listBookingList.setLayoutManager(mLayoutManager)
@@ -60,6 +66,21 @@ class BookingStatusActivity : AirTricketBaseActivity() {
         tvBookingStatus.typeface = Typeface.DEFAULT_BOLD
         tvAction.typeface = Typeface.DEFAULT_BOLD
 
+
+    }
+
+    private fun showTricketIntentPopupMessage() {
+        val builder = AlertDialog.Builder(this)
+        builder.setMessage(getString(R.string.please_seleted_a_option))
+                .setCancelable(true)
+                .setPositiveButton(getString(R.string.view_tricket), DialogInterface.OnClickListener { dialog, id ->
+
+                })
+                .setNegativeButton(getString(R.string.email), DialogInterface.OnClickListener { dialog, id ->
+
+                })
+        val alert = builder.create()
+        alert.show()
 
     }
 
@@ -78,67 +99,5 @@ class BookingStatusActivity : AirTricketBaseActivity() {
         finish()
     }
 
-    inner class MyAdapter : RecyclerView.Adapter<ViewHolder> {
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-            val view = mInflater!!.inflate(com.cloudwell.paywell.services.R.layout.item_booking_status, parent, false)
-            return ViewHolder(view)
-        }
 
-        override fun getItemCount(): Int {
-            return responseList.data.size
-        }
-
-        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-            val count = position + 1
-
-            val inputFormat = SimpleDateFormat("yyyy-MM-dd hh:mm:ss", Locale.ENGLISH)
-            val outputFormat = SimpleDateFormat("dd/MM/yy", Locale.ENGLISH)
-            val inputDateStr = responseList.data.get(position).firstRequestDateTime
-            val date = inputFormat.parse(inputDateStr)
-            val outputDateStr = outputFormat.format(date)
-
-            holder.tvSerialNumber.setText("" + count)
-            holder.tvDate.setText(responseList.data.get(position).bookingId)
-            holder.tvDate.setText(outputDateStr)
-            holder.tvBookingStatus.setText(responseList.data.get(position).message)
-            holder.tvBookingId.setText(responseList.data.get(position).bookingId)
-
-
-            if (position % 2 == 0)
-                holder.rootLayout_booking_status.setBackgroundColor(Color.parseColor("#d8ead2"))
-            else
-                holder.rootLayout_booking_status.setBackgroundColor(Color.parseColor("#b0d5a4"))
-
-
-        }
-
-        var context: Context? = null
-
-        private var mInflater: LayoutInflater?
-
-        constructor(context: Context) {
-            this.context = context
-            this.mInflater = LayoutInflater.from(context)
-        }
-
-
-    }
-
-
-    interface ItemClickListener {
-
-        fun onItemClick(position: Int, favoriteMenu1: FavoriteMenu)
-    }
-
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        // Holds the TextView that will add each animal to
-        // Holds the TextView that will add each animal to
-        val rootLayout_booking_status = view.rootLayout_booking_status
-        val tvSerialNumber = view.tvSerialNumber
-        val tvBookingId = view.tvBookingId
-        val tvDate = view.tvDate
-        val tvBookingStatus = view.tvBookingStatus
-        val tvAction = view.tvAction
-
-    }
 }
---
+++ paywellservices/src/main/java/com/cloudwell/paywell/services/activity/eticket/airticket/bookingStatus/BookingStatusListAdapter.kt
@@ -0,0 +1,90 @@
+package com.cloudwell.paywell.services.activity.eticket.airticket.bookingStatus
+
+
+import android.content.Context
+import android.graphics.Color
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.cloudwell.paywell.services.activity.eticket.airticket.booking.model.BookingList
+import com.cloudwell.paywell.services.activity.eticket.airticket.booking.model.Datum
+import kotlinx.android.synthetic.main.item_booking_status.view.*
+import java.text.SimpleDateFormat
+import java.util.*
+
+/**
+ * Created by Kazi Md. Saidul Email: Kazimdsaidul@gmail.com  Mobile: +8801675349882 on 25/3/19.
+ */
+class BookingStatusListAdapter(val responseList: BookingList, val context: Context, val itemClickListener: ItemClickListener) : RecyclerView.Adapter<ViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(com.cloudwell.paywell.services.R.layout.item_booking_status, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return responseList.data.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val count = position + 1
+
+        val model = responseList.data.get(position)
+
+        val inputFormat = SimpleDateFormat("yyyy-MM-dd hh:mm:ss", Locale.ENGLISH)
+        val outputFormat = SimpleDateFormat("dd/MM/yy", Locale.ENGLISH)
+        val inputDateStr = model.firstRequestDateTime
+        val date = inputFormat.parse(inputDateStr)
+        val outputDateStr = outputFormat.format(date)
+
+        holder.tvSerialNumber.setText("" + count)
+        holder.tvDate.setText(model.bookingId)
+        holder.tvDate.setText(outputDateStr)
+        holder.tvBookingStatus.setText(model.message)
+        holder.tvBookingId.setText(model.bookingId)
+
+        if (model.message != null) {
+            if (model.message.equals("Ticketed")) {
+                holder.ivSymbolTicketed.visibility = View.VISIBLE
+            } else {
+                holder.ivSymbolTicketed.visibility = View.INVISIBLE
+            }
+        } else {
+            holder.ivSymbolTicketed.visibility = View.INVISIBLE
+        }
+
+        holder.tvBookingId.setOnClickListener {
+            if (model.equals("Ticketed")) {
+                itemClickListener.onItemClick(model)
+            }
+        }
+
+        if (position % 2 == 0)
+            holder.rootLayout_booking_status.setBackgroundColor(Color.parseColor("#d8ead2"))
+        else
+            holder.rootLayout_booking_status.setBackgroundColor(Color.parseColor("#b0d5a4"))
+
+    }
+
+}
+
+
+interface ItemClickListener {
+
+    fun onItemClick(position: Datum)
+}
+
+class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    val rootLayout_booking_status = view.rootLayout_booking_status
+    val tvSerialNumber = view.tvSerialNumber
+    val tvBookingId = view.tvBookingId
+    val tvDate = view.tvDate
+    val tvBookingStatus = view.tvBookingStatus
+    val tvAction = view.tvAction
+    val ivSymbolTicketed = view.ivSymbolTicketed
+
+}
