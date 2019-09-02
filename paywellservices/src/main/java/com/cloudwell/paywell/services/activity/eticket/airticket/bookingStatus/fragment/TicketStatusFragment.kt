c242d1f46b389fc4fe054f6bf97c39614a5efe10
Kazi md. Saidul <kazi.saidul@cloudwell.co>
2019-09-02 14:51:47 +0600

    bug fixed ticket status fragment old view id

--- paywellservices/src/main/java/com/cloudwell/paywell/services/activity/eticket/airticket/bookingStatus/fragment/TicketStatusFragment.kt
+++ paywellservices/src/main/java/com/cloudwell/paywell/services/activity/eticket/airticket/bookingStatus/fragment/TicketStatusFragment.kt
@@ -5,7 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
-import kotlinx.android.synthetic.main.fragment_prices_change.view.*
+import kotlinx.android.synthetic.main.fragment_status_cencelation.view.*
 
 
 class TicketStatusFragment : DialogFragment() {
@@ -25,10 +25,10 @@ class TicketStatusFragment : DialogFragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val v = inflater.inflate(com.cloudwell.paywell.services.R.layout.fragment_status_cencelation, container, false)
 
-        v.tvYourSeats.text = message
+        v.tvFree.text = message
 
 
-        v.btAction.setOnClickListener {
+        v.btActionIssueTicket.setOnClickListener {
             dismiss()
 
         }
