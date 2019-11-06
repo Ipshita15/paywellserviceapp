d15fa6aaf3b2dc7a999643e51e6bf38ff44b61bf
Kazi md. Saidul <kazi.saidul@cloudwell.co>
2019-11-06 16:34:22 +0600

    Add balance refile instruction first line

--- paywellservices/src/main/java/com/cloudwell/paywell/services/activity/refill/nagad/BalanceRefillFragment.java
+++ paywellservices/src/main/java/com/cloudwell/paywell/services/activity/refill/nagad/BalanceRefillFragment.java
@@ -1,13 +1,15 @@
 package com.cloudwell.paywell.services.activity.refill.nagad;
 
 import android.os.Bundle;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import androidx.fragment.app.Fragment;
+import android.widget.TextView;
 
 import com.cloudwell.paywell.services.R;
+
+import androidx.fragment.app.Fragment;
 
 public class BalanceRefillFragment extends Fragment {
 
@@ -23,7 +25,21 @@ public class BalanceRefillFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.balance_refill_fragment, container, false);
+        View view = inflater.inflate(R.layout.balance_refill_fragment, container, false);
+        TextView tvView = view.findViewById(R.id.tvLine1);
+
+        String next = "নগদ অ্যাপ-এর মার্চেন্ট পে মেনুতে গিয়ে "+getFontStyle("QR")+" কোড স্ক্যান করুন বা ১১ ডিজিটের মোবাইল নাম্বারে "+getFontStyle("01787679661")+"  লিখে বাটনটি ট্যাপ করুন। টাকার পরিমাণ লিখে পরবর্তী ধাপে যান।";
+        tvView.setText(Html.fromHtml(next));
+
+
+
+        return view;
     }
+
+    private String getFontStyle(String input) {
+        String textColor = "#5aac40";
+        return "<b><font color='"+textColor+"'>"+input+"</font></b>";
+    }
+
+
 }
