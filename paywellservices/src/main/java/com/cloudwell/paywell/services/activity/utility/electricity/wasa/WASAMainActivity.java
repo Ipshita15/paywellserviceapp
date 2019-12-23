4a5c1b946dc82c91516ff30d2eaf2b8930405b8b
Israt Jahan <jahan.misu1@gmail.com>
2019-12-23 16:55:34 +0600

    added ref_id for WASA service

--- paywellservices/src/main/java/com/cloudwell/paywell/services/activity/utility/electricity/wasa/WASAMainActivity.java
+++ paywellservices/src/main/java/com/cloudwell/paywell/services/activity/utility/electricity/wasa/WASAMainActivity.java
@@ -19,6 +19,8 @@ import com.cloudwell.paywell.services.app.AppController;
 import com.cloudwell.paywell.services.app.AppHandler;
 import com.cloudwell.paywell.services.constant.AllConstant;
 import com.cloudwell.paywell.services.utils.ConnectionDetector;
+import com.cloudwell.paywell.services.utils.ParameterUtility;
+import com.cloudwell.paywell.services.utils.UniqueKeyGenerator;
 import com.google.android.material.snackbar.Snackbar;
 
 import org.apache.http.NameValuePair;
@@ -233,6 +235,8 @@ public class WASAMainActivity extends BaseActivity implements CompoundButton.OnC
         @Override
         protected String doInBackground(String... params) {
             String responseTxt = null;
+
+            String uniqueKey = UniqueKeyGenerator.getUniqueKey(AppHandler.getmInstance(WASAMainActivity.this).getRID());
             // Create a new HttpClient and Post Header
             HttpClient httpclient = new DefaultHttpClient();
             HttpPost httppost = new HttpPost(params[0]);
@@ -241,6 +245,7 @@ public class WASAMainActivity extends BaseActivity implements CompoundButton.OnC
                 nameValuePairs.add(new BasicNameValuePair("username", mAppHandler.getImeiNo()));
                 nameValuePairs.add(new BasicNameValuePair("service", "WASA"));
                 nameValuePairs.add(new BasicNameValuePair("limit", selectedLimit));
+                nameValuePairs.add(new BasicNameValuePair(ParameterUtility.KEY_REF_ID, uniqueKey));
                 httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
 
                 ResponseHandler<String> responseHandler = new BasicResponseHandler();
