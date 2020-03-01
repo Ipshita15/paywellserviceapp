3aeba0c737d3b2c1af321d8b29dc40df4f0c8c35
Kazi md. Saidul <kazimdsaidul@gmail.com>
2020-03-01 16:22:48 +0600

    Bug fixed:  Desco prepaid inquiry not working

--- paywellservices/src/main/java/com/cloudwell/paywell/services/activity/utility/electricity/desco/prepaid/DescoPrepaidMainActivity.java
+++ paywellservices/src/main/java/com/cloudwell/paywell/services/activity/utility/electricity/desco/prepaid/DescoPrepaidMainActivity.java
@@ -231,7 +231,7 @@ public class DescoPrepaidMainActivity extends BaseActivity implements CompoundBu
 
         DescoPrepaidTrxLogRequest descoPrepaidTrxLogRequest =new DescoPrepaidTrxLogRequest("json",selectedLimit,uniqueKey,"Desco_prepaid",imeiNo);
 
-        ApiUtils.getAPIService().descoPrepaidTrxInquiry(descoPrepaidTrxLogRequest).enqueue(new Callback<DescoPrepaidTrxLogResponse>() {
+        ApiUtils.getAPIServiceV2().descoPrepaidTrxInquiry(descoPrepaidTrxLogRequest).enqueue(new Callback<DescoPrepaidTrxLogResponse>() {
             @Override
             public void onResponse(Call<DescoPrepaidTrxLogResponse> call, Response<DescoPrepaidTrxLogResponse> response) {
 
