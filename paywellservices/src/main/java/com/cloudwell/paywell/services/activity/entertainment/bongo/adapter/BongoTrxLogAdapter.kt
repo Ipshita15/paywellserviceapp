27853022c06a8e84f58a4a50f640b92b032eb532
ismail <ismail.hossain@cloudwell.co>
2020-05-20 23:06:09 +0600

    Bongo Transcation APi Change

--- paywellservices/src/main/java/com/cloudwell/paywell/services/activity/entertainment/bongo/adapter/BongoTrxLogAdapter.kt
+++ paywellservices/src/main/java/com/cloudwell/paywell/services/activity/entertainment/bongo/adapter/BongoTrxLogAdapter.kt
@@ -1,18 +1,19 @@
 package com.cloudwell.paywell.services.activity.entertainment.bongo.adapter
 
 import android.content.Context
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.cloudwell.paywell.services.R
 import com.cloudwell.paywell.services.activity.education.bbc.model.CoursesItem
-import com.cloudwell.paywell.services.activity.entertainment.bongo.model.DataItem
+import com.cloudwell.paywell.services.activity.entertainment.bongo.model.ResponseDetailsItem
 import kotlinx.android.synthetic.main.bbc_trx_item.view.txr_name
 import kotlinx.android.synthetic.main.bongo_trx_item.view.*
 
 
-class BongoTrxLogAdapter(val mContext: Context, var trList: List<DataItem?>?) : RecyclerView.Adapter<BongoTrxLogAdapter.CourseListViewHolder>() {
+class BongoTrxLogAdapter(val mContext: Context, var trList: ArrayList<ResponseDetailsItem?>?) : RecyclerView.Adapter<BongoTrxLogAdapter.CourseListViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CourseListViewHolder {
         val v: View = LayoutInflater.from(parent?.context)
@@ -27,11 +28,18 @@ class BongoTrxLogAdapter(val mContext: Context, var trList: List<DataItem?>?) : 
 
     override fun onBindViewHolder(holder: CourseListViewHolder, position: Int) {
 
-        holder.bongo_date.setText(trList?.get(position)?.addtime)
-        holder.bongo_status.setText(trList?.get(position)?.statusName)
+        holder.bongo_date.setText(trList?.get(position)?.addDatetime)
         holder.bongo_customerNumber.setText(trList?.get(position)?.customerMobileNo)
-        holder.amount.setText(trList?.get(position)?.totalAmount)
         holder.courseSubscriberName.setText(trList?.get(position)?.title)
+        val status = trList?.get(position)?.statusName
+        if (status.equals("Successful")){
+            holder.bongo_status.setText(status)
+            holder.bongo_status.setTextColor(mContext.resources.getColor(R.color.tab_background))
+        }else{
+            holder.bongo_status.setText(status)
+            holder.bongo_status.setTextColor(Color.RED)
+        }
+
 
     }
 
@@ -41,7 +49,6 @@ class BongoTrxLogAdapter(val mContext: Context, var trList: List<DataItem?>?) : 
         val bongo_customerNumber = itemView.bongo_number
         val bongo_status = itemView.bongo_status
         val bongo_date = itemView.bongo_trx_date
-        val amount = itemView.amount_bongo
 
     }
 
