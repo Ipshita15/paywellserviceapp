93574d5256b3fede94883d7278f0aae087514f3a
Kazi Md. Saidul <kazimdsaidul@gmail.com>
2020-12-08 22:35:45 +0600

    check build version

--- paywellservices/src/main/java/com/cloudwell/paywell/services/activity/MainActivity.java
+++ paywellservices/src/main/java/com/cloudwell/paywell/services/activity/MainActivity.java
@@ -54,6 +54,7 @@ import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.cloudwell.paywell.services.BuildConfig;
 import com.cloudwell.paywell.services.R;
 import com.cloudwell.paywell.services.activity.about.AboutActivity;
 import com.cloudwell.paywell.services.activity.base.BaseActivity;
@@ -374,9 +375,9 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
 
         try {
             String rid = getString(R.string.rid) + mAppHandler.getRID();
-//            if (BuildConfig.DEBUG) {
-            rid = rid + "(" + AppTestVersionUtility.testVersion + ")";
-//            }
+            if (BuildConfig.DEBUG) {
+                rid = rid + "(" + AppTestVersionUtility.testVersion + ")";
+            }
             _pwId.setText(rid);
             if (mAppHandler.getAppLanguage().equalsIgnoreCase("en")) {
                 _pwId.setTypeface(AppController.getInstance().getOxygenLightFont());
